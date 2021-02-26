ec922abafb5a5167bb036f266823b0dff930b433
yeegor <yegor.batov@gmail.com>
2021-02-26 19:14:33 +0200

    Improve module typings for the config injector

--- integration/config-injectors/typings/index.d.ts
+++ integration/config-injectors/typings/index.d.ts
@@ -1,14 +1,19 @@
-export interface WebpackInjectorConfig {
-    // Inject definitions into ProvidePlugin
-    provideGlobals: boolean
+declare module '@plugjs/config-injectors' {
+    export interface WebpackInjectorConfig {
+        // Inject definitions into ProvidePlugin
+        provideGlobals: boolean
+    
+        // Support util/extensions
+        supportLegacy: boolean,
+    
+        // They may be several entry points for plugins
+        // These files will be injected with ExtUtils.setPlugins
+        entryMatcher: string | ((filepath: string) => boolean) | RegExp,
+    
+        // Instance of webpack that the application uses
+        webpack: any
+    }
 
-    // Support util/extensions
-    supportLegacy: boolean,
-
-    // They may be several entry points for plugins
-    // These files will be injected with ExtUtils.setPlugins
-    entryMatcher: string | function | RegExp,
-
-    // Instance of webpack that the application uses
-    webpack: any
+    export function injectBabelConfig(babelConfig: object): object;
+    export function injectWebpackConfig(webpackConfig: object, options: WebpackInjectorConfig): object;
 }
