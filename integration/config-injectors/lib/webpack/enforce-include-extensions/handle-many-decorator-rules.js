72d1668d6485e15b02295d5329fa7c6e7410a363
yeegor <yegor.batov@gmail.com>
2021-03-03 10:45:02 +0200

    Fix context leak

--- integration/config-injectors/lib/webpack/enforce-include-extensions/handle-many-decorator-rules.js
+++ integration/config-injectors/lib/webpack/enforce-include-extensions/handle-many-decorator-rules.js
@@ -7,7 +7,7 @@ const potentiallyNamespacedFiles = require('./potentially-namespaced-files');
  * How to handle this is ambiguous, we'll leave it to the user himself
  * This case should not be encountered frequently
  */
-const handleManyDecoratorRules = () => {
+const handleManyDecoratorRules = (middlewareDecoratorRules, webpackConfig) => {
     const excludedExtensionFile = potentiallyNamespacedFiles.find(
         (includePath) => !middlewareDecoratorRules.find(
             (rule) => getRuleAppliesToFile(rule, includePath)
