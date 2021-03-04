481485cb79fa8a071fbee2a5e861ad5fcf3708a9
yeegor <yegor.batov@gmail.com>
2021-03-04 16:19:57 +0200

    Support webpack configs as promises and functions

--- integration/config-injectors/lib/webpack/index.js
+++ integration/config-injectors/lib/webpack/index.js
@@ -10,13 +10,7 @@ const defaultOptions = {
     supportLegacy: false
 };
 
-/**
- * Inject webpack configuration with necessary things for the e11y package
- *
- * @param {any} webpackConfig
- * @param {import('@plugjs/config-injectors').WebpackInjectorConfig} providedOptions
- */
-const injectWebpackConfig = (
+const injectWebpackConfigObject = (
     webpackConfig,
     providedOptions = {}
 ) => {
@@ -42,6 +36,39 @@ const injectWebpackConfig = (
     }
 
     return webpackConfig;
+}
+
+/**
+ * Inject webpack configuration with necessary things for the e11y package
+ *
+ * @param {any} webpackConfig
+ * @param {import('@plugjs/config-injectors').WebpackInjectorConfig} providedOptions
+ */
+const injectWebpackConfig = (webpackConfig, providedOptions) => {
+    // Handle function configs
+    if (typeof webpackConfig === 'function') {
+        return (env, argv) => injectWebpackConfigObject(webpackConfig(env, argv), providedOptions);
+    } 
+    
+    // Handle promise configs
+    if (webpackConfig instanceof Promise) {
+        return webpackConfig.then((config) => injectWebpackConfigObject(config, providedOptions));
+    } 
+    
+    // Handle arrays (multi-compiler mode)
+    if (Array.isArray(webpackConfig)) {
+        throw new TypeError(
+            'Array configurations are not supported! '+
+            'You may apply this injector manually to each config item you consider necessary to inject.'
+        )
+    }
+
+    // Handle regular configs
+    if (typeof webpackConfig === 'object') {
+        return injectWebpackConfigObject(webpackConfig, providedOptions)
+    } 
+
+    throw new TypeError(`Unexpected type of webpack configuration: ${typeof webpackConfig}!`);
 };
 
 module.exports = injectWebpackConfig;
