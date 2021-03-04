1b301ab006a9fd6b2804132b04cf65fb5cfa7285
yeegor <yegor.batov@gmail.com>
2021-03-04 15:23:06 +0200

    Add module linking for node projects

---
+++ integration/node-scripts/bin/node-scripts.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const linkExtensions = require('@plugjs/dev-utils/link-extensions');
+
+const args = process.argv.slice(2);
+
+const scriptMap = {
+    link: linkExtensions.bind(null, process.cwd())
+};
+
+const scriptIndex = args.findIndex((x) => Object.keys(scriptMap).includes(x));
+const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
+
+scriptMap[script]();
