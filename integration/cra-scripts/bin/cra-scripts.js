d3caf78b7325b56d4be69f738ad10bf7b07a1a8f
yeegor <yegor.batov@gmail.com>
2021-03-05 13:18:06 +0200

    Add testing command

--- integration/cra-scripts/bin/cra-scripts.js
+++ integration/cra-scripts/bin/cra-scripts.js
@@ -1,17 +1,26 @@
 #!/usr/bin/env node
 
 const linkExtensions = require('@plugjs/dev-utils/link-extensions');
+const logger = require('@plugjs/dev-utils/logger');
 const runCraco = (...args) => require('../lib/craco')(...args);
+const runTests = () => require('react-scripts/scripts/test');
 
 const args = process.argv.slice(2);
 
-const scriptIndex = args.findIndex((x) => x === 'build' || x === 'start');
-const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
+const scriptToRun = args[0];
+const pureArgs = args.slice(1);
 
 const scriptMap = {
     build: runCraco,
     start: runCraco,
-    link: linkExtensions.bind(null, process.cwd())
+    link: linkExtensions.bind(null, process.cwd()),
+    test: runTests
 };
 
-scriptMap[script]();
+const script = scriptMap[scriptToRun];
+if (!script) {
+    logger.error(`Script not found: ${scriptToRun}`);
+    process.exit(1);
+}
+
+script(...pureArgs);
