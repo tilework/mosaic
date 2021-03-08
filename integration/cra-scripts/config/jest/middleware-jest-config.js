0fc1c6fd41ddc174a98b33134d10aa060c4cee25
yeegor <yegor.batov@gmail.com>
2021-03-08 15:07:12 +0200

    Include tests located within external packages

--- integration/cra-scripts/config/jest/middleware-jest-config.js
+++ integration/cra-scripts/config/jest/middleware-jest-config.js
@@ -1,3 +1,6 @@
+const extensions = require('@plugjs/dev-utils/extensions');
+const path = require('path');
+
 const provideProperBabelTransform = (jestConfig) => {
     const jsTransformKey = Object.keys(jestConfig.transform).find(
         (key) => new RegExp(key).test('sample.js')
@@ -10,9 +13,24 @@ const provideGlobals = (jestConfig) => {
     jestConfig.setupFiles.push(require.resolve('./provideGlobals'));
 }
 
+const includeExternals = (jestConfig) => {
+    const modulesRelativePaths = extensions.map(
+        ({ packagePath }) => path.relative(process.cwd(), packagePath)
+    );
+
+    jestConfig.rootDir = process.cwd();
+    jestConfig.roots.push(...modulesRelativePaths.map(
+        (moduleRelative) => ['<rootDir>', moduleRelative].join(path.sep)
+    ));
+    jestConfig.testMatch = jestConfig.testMatch.map(
+        (match) => match.replace(/\<rootDir\>\/?/, '**/')
+    );
+}
+
 const middlewareJestConfig = (jestConfig) => {
     provideProperBabelTransform(jestConfig);
     provideGlobals(jestConfig);
+    includeExternals(jestConfig);
 
     return jestConfig;
 };
