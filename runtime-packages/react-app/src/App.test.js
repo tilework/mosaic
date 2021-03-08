b67969173d69632ddb937e4aadff89de174f5a4a
yeegor <yegor.batov@gmail.com>
2021-03-08 12:42:26 +0200

    Modify test sample to include namespace testing

--- runtime-packages/react-app/src/App.test.js
+++ runtime-packages/react-app/src/App.test.js
@@ -1,5 +1,12 @@
 import { render, screen } from '@testing-library/react';
+import util from 'util';
 import App from './App';
+
+test('is proxy', () => {
+  const isProxy = util.types.isProxy(App);
+
+  expect(isProxy).toBe(true);
+})
 
 test('renders learn react link', () => {
   render(<App />);
