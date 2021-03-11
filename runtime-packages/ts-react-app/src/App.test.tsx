4ef5ee7c16dea63a1371e073c684681e054de1e7
yeegor <yegor.batov@gmail.com>
2021-03-11 11:41:19 +0200

    Update tests

--- runtime-packages/ts-react-app/src/App.test.tsx
+++ runtime-packages/ts-react-app/src/App.test.tsx
@@ -1,6 +1,12 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import util from 'util';
+
+test('is proxy in local test', () => {
+  const isProxy = util.types.isProxy(App);
+
+  expect(isProxy).toBe(true);
+})
 
 test('renders learn react link', () => {
   render(<App />);
