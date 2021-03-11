342573f89d6543c0e21076528b1cc252d8245afd
yeegor <yegor.batov@gmail.com>
2021-03-11 18:43:30 +0200

    Add and refactor alias-related functionality

---
+++ integration/config-injectors/lib/babel/add-aliases.js
@@ -0,0 +1,20 @@
+const { alias } = require('./util/alias');
+
+const addAliases = (babelConfig) => {
+    if (!babelConfig.plugins) {
+        babelConfig.plugins = [];
+    }
+
+    babelConfig.plugins.push([
+        require.resolve('babel-plugin-module-resolver'), 
+        {
+            root: 'src',
+            loglevel: 'silent',
+            alias
+        }
+    ]);
+
+    return babelConfig;
+}
+
+module.exports = addAliases;
