d6ae28c029e2d95a461d6437330ec3cbc2e475b4
yeegor <yegor.batov@gmail.com>
2021-03-11 18:46:39 +0200

    Ensure [j|t]sconfig processing during the Babel injection

--- integration/config-injectors/lib/babel/index.js
+++ integration/config-injectors/lib/babel/index.js
@@ -1,5 +1,17 @@
 const addPlugins = require('./add-plugins');
+const addAliases = require('./add-aliases');
+const ensureConfig = require('./ensure-config');
 
-const injectBabelConfig = (babelConfig) => addPlugins(babelConfig);
+const injectBabelConfig = (babelConfig) => {
+    addPlugins(babelConfig);
+    addAliases(babelConfig);
+
+    // (j|t)sconfigs are necessary only when Babel aliases are present
+    // In other scenarios, they get generated properly without our help
+    // Hence, we keep it here.
+    ensureConfig();
+
+    return babelConfig;
+};
 
 module.exports = injectBabelConfig;
