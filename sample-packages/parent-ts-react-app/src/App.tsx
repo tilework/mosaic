c2b137b116e3eb6df9b4d6e8e9e7e640fe1238eb
yeegor <yegor.batov@gmail.com>
2021-04-08 15:41:27 +0300

    Add namespace to the TS app component

--- sample-packages/parent-ts-react-app/src/App.tsx
+++ sample-packages/parent-ts-react-app/src/App.tsx
@@ -2,6 +2,7 @@ import './index.css';
 
 import { PureComponent } from 'react';
 
+/** @namespace ParentTsReactApp/App */
 class App extends PureComponent<{}, {}> {
   renderContent() {
     return <p>This is written in the parent theme in TS</p>;
