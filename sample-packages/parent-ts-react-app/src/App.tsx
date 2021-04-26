46993e17214ceffb892049a557db6b459eafa3ab
yeegor <yegor.batov@gmail.com>
2021-04-26 16:02:54 +0300

    Better extensions' contents

--- sample-packages/parent-ts-react-app/src/App.tsx
+++ sample-packages/parent-ts-react-app/src/App.tsx
@@ -3,18 +3,18 @@ import './index.css';
 import { PureComponent } from 'react';
 
 /** @namespace ParentTsReactApp/App */
-class App extends PureComponent<{}, {}> {
-  renderContent() {
-    return <p>This is written in the parent theme in TS</p>;
-  }
+class App extends PureComponent {
+    renderContent(): React.ReactNode {
+        return <p>This is written in the parent theme in TS</p>;
+    }
 
-  render() {
-    return (
-      <div className="Application">
-        { this.renderContent() }
-      </div>
-    );
-  }
+    render(): React.ReactNode {
+        return (
+            <div className="Application">
+                { this.renderContent() }
+            </div>
+        );
+    }
 }
 
 export default App;
