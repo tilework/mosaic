5215ea7c0273c4b8a4fed314617b76cd77acbcba
yeegor <yegor.batov@gmail.com>
2021-05-25 14:53:31 +0300

    Proper instance processing in construct

--- core/mosaic/lib/handlers/construct.js
+++ core/mosaic/lib/handlers/construct.js
@@ -1,6 +1,42 @@
 import generateMiddlewaredClass from '../middleware/class';
 import getPluginsFromConfig from '../plugins/get-plugins';
 import getWrapperFromPlugin from '../plugins/get-wrapper-from-plugin';
+import getNamespacesFromMiddlewarable from '../namespace/get-namespaces-from-middlewarable';
+
+function postProcessInstance(instance, constructorArguments) {
+    const namespaces = getNamespacesFromMiddlewarable(Object.getPrototypeOf(instance).constructor);
+
+    // Get all member-property plugins
+    const namespacesPluginsConstruct = getPluginsFromConfig(namespaces, 'member-property');
+
+    // Handle plugin -> property interactions
+    namespacesPluginsConstruct.forEach(
+        (namespacePluginsConstruct) => Object.entries(namespacePluginsConstruct).forEach(
+            // Apply each plugin to the instance
+            ([memberName, memberPluginsConstruct]) => {
+                // Retrieve the original member
+                const origMember = instance[memberName] || (() => {});
+
+                // Wrap it into the plugins
+                const newMember = memberPluginsConstruct.reduce(
+                    (acc, plugin) => getWrapperFromPlugin(plugin, origMember.name)(acc, instance),
+                    origMember
+                );
+
+                // Replace the original member with the new one, wrapped into the plugins
+                instance[memberName] = newMember;
+            }
+        )
+    );
+
+    // Handle construct logic
+    if (instance.__construct) {
+        // Call the "magic" __construct member function
+        instance.__construct(...constructorArguments);
+    }
+
+    return instance;
+}
 
 export default function generateConstructHandler(namespaces) {
     return (TargetClass, args, newTarget) => {
@@ -8,46 +44,18 @@ export default function generateConstructHandler(namespaces) {
         const WrappedClass = generateMiddlewaredClass(
             // Preserve the original prototypes: prevent React's confusion
             class extends TargetClass {
-                constructor() {
-                    return Reflect.construct(TargetClass, args, newTarget);
+                constructor(...childArgs) {
+                    // `super` is not called deliberately
+                    const instance = Reflect.construct(TargetClass, args, newTarget);
+
+                    return postProcessInstance(instance, childArgs);
                 }
             },
             namespaces
         );
 
         // Get an instance
-        // const instance = Reflect.construct(WrappedClass, args);
         const instance = Reflect.construct(WrappedClass, args);
-
-        // Get all member-property plugins
-        const namespacesPluginsConstruct = getPluginsFromConfig(namespaces, 'member-property');
-
-        // Handle plugin -> property interactions
-        namespacesPluginsConstruct.forEach(
-            (namespacePluginsConstruct) => Object.entries(namespacePluginsConstruct).forEach(
-                // Apply each plugin to the instance
-                ([memberName, memberPluginsConstruct]) => {
-                    // Retrieve the original member
-                    const origMember = instance[memberName] || (() => {});
-                    const sortedPlugins = memberPluginsConstruct;
-
-                    // Wrap it into the plugins
-                    const newMember = sortedPlugins.reduce(
-                        (acc, plugin) => getWrapperFromPlugin(plugin, origMember.name)(acc, instance),
-                        origMember
-                    );
-
-                    // Replace the original member with the new one, wrapped into the plugins
-                    instance[memberName] = newMember;
-                }
-            )
-        );
-
-        // Handle construct logic
-        if (instance.__construct) {
-            // Call the "magic" __construct member function
-            instance.__construct(...args);
-        }
 
         // Return the processed instance
         return instance;
