a872ac0d1be8e809b4dd3084e217a7a5de8f6e7f
yeegor <yegor.batov@gmail.com>
2021-05-28 13:03:47 +0300

    Ensure single Mosaic in the application

--- integration/mosaic-config-injectors/lib/webpack/provide-globals.js
+++ integration/mosaic-config-injectors/lib/webpack/provide-globals.js
@@ -1,7 +1,21 @@
 const logger = require('@tilework/mosaic-dev-utils/logger');
 
+const getMosaicPath = () => {
+    // Get the one from CWD
+    try {
+        return require.resolve('@tilework/mosaic', { paths: [process.cwd()] });
+    } catch (err) {
+        logger.error(
+            `${logger.style.misc('@tilework/mosaic')} cannot be resolved from the CWD. Please install ${logger.style.misc('@tilework/mosaic')} into the main theme.`,
+            `This way all of your packages are going to use the same version of ${logger.style.misc('@tilework/mosaic')}`
+        );
+
+        process.exit(0);
+    }
+}
+
 const additionalProvideDefinitions = {
-    Mosaic: ['@tilework/mosaic/dist/mosaic.js']
+    Mosaic: getMosaicPath()
 };
 
 /**
