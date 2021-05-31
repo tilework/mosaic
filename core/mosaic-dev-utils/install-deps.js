6ebbe581766a05b90ec0443c6918884eeab7f83a
yeegor <yegor.batov@gmail.com>
2021-05-31 13:54:19 +0300

    Remove redundant dev utils

--- core/mosaic-dev-utils/install-deps.js
+++
@@ -1,9 +0,0 @@
-const shouldUseYarn = require('./should-use-yarn');
-const execCommandAsync = require('./exec-command');
-
-const installDeps = (pathname) => {
-    const command = shouldUseYarn() ? 'yarnpkg' : 'npm';
-    return execCommandAsync(command, ['install'], pathname);
-};
-
-module.exports = installDeps;
