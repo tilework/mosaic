f514b5c9d27270445ab3b387071c16f2158268e2
yeegor <yegor.batov@gmail.com>
2021-07-02 14:57:37 +0300

    Multiple plugins for static members

--- core/mosaic/lib/middleware/function.js
+++ core/mosaic/lib/middleware/function.js
@@ -3,13 +3,19 @@ import getWrapperFromPlugin from '../plugins/get-wrapper-from-plugin';
 export default function generateMiddlewaredFunction(origMember = () => {}, sortedPlugins, origContext) {
     return (...args) => {
         const newMember = sortedPlugins.reduce(
+            // A function is always generateed
             (acc, plugin) => () => {
                 const wrapper = getWrapperFromPlugin(plugin, origMember.name);
 
                 // Provide different arguments due to API difference
                 // Between property and function call interception
                 return typeof origMember === 'object'
-                    ? wrapper(acc, origContext)
+                    ? wrapper(
+                        // This reduce always generates a function
+                        // Hence, if we operate with a prop -> call the function to get it
+                        typeof acc === 'function' ? acc() : acc,
+                        origContext
+                    )
                     : wrapper(
                         args,
                         acc.bind(origContext),
