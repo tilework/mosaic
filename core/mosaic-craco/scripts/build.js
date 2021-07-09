f4331312ccbc4c418a276bab1c322d291707b0d7
Alexey Glazkov <glazkov.dev@outlook.com>
2021-07-09 14:23:54 +0300

    Apply base initialization script to start, build and test scripts

--- core/mosaic-craco/scripts/build.js
+++ core/mosaic-craco/scripts/build.js
@@ -1,29 +1,17 @@
 process.env.NODE_ENV = "production";
 
-const { findArgsFromCli } = require("../lib/args");
-
-// Make sure this is called before "paths" is imported.
-findArgsFromCli();
-
-const { log } = require("../lib/logger");
-const { getCraPaths, build } = require("../lib/cra");
-const { loadCracoConfigAsync } = require("../lib/config");
 const { overrideWebpackProd } = require("../lib/features/webpack/override");
-const { overrideCraPaths } = require("../lib/features/cra-paths/override");
 const { validateCraVersion } = require("../lib/validate-cra-version");
+const { build } = require("../lib/cra");
+const { initialize } = require("./script");
 
-log("Override started with arguments: ", process.argv);
-log("For environment: ", process.env.NODE_ENV);
+const { craco, context } = initialize();
 
-const context = {
-    env: process.env.NODE_ENV
-};
+craco.then(
+    (craco) => {
+        validateCraVersion(cracoConfig);
 
-loadCracoConfigAsync(context).then(cracoConfig => {
-    validateCraVersion(cracoConfig);
-
-    context.paths = getCraPaths(cracoConfig);
-    overrideCraPaths(cracoConfig, context);
-    overrideWebpackProd(cracoConfig, context);
-    build(cracoConfig);
-});
+        overrideWebpackProd(craco, context);
+        build(craco);
+    }
+);
