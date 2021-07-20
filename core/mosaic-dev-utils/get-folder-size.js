36b4e497d36bc006164df2ff5355b6c3e3e84d04
AleksandrsKondratjevs <Aleksandrs.Kondratjevs@scandiweb.com>
2021-07-20 18:25:55 +0300

    Use Promise.all for parallel calculation

--- core/mosaic-dev-utils/get-folder-size.js
+++ core/mosaic-dev-utils/get-folder-size.js
@@ -4,19 +4,34 @@ const fs = require('fs');
 const path = require('path');
 
 const getFolderSizeFn = async (dirPath, size = 0) => {
-    let fSize = 0;
+    let result = 0;
+    const fFiles = [];
 
     for (const file of await fs.promises.readdir(dirPath, { withFileTypes: true, encoding: 'utf-8' })) {
         const filePath = path.join(dirPath, file.name);
 
         if (file.isFile()) {
-            fSize += (await fs.promises.stat(filePath)).size;
+            fFiles.push(fs.promises.stat(filePath));
         } else if (file.isDirectory()) {
-            fSize += await getFolderSizeFn(filePath);
+            fFiles.push(await getFolderSizeFn(filePath));
         }
     }
 
-    return size + fSize;
+    // Execute all promises and sum size depending on if file
+    // or result from previous calculation
+    await Promise.all(fFiles).then((values) => {
+        result = size + values.reduce((acc, file) => {
+            if (Number.isInteger(file)) {
+                return acc + file;
+            }
+
+            const { size } = file;
+
+            return acc + size;
+        }, 0);
+    });
+
+    return result;
 };
 
 const getFolderSize = async (dirPath) => Math.round(await getFolderSizeFn(dirPath) / 1024 / 1024);
