a459e9c4b88f018a94090d686e70b10cd780892f
AleksandrsKondratjevs <Aleksandrs.Kondratjevs@scandiweb.com>
2021-07-21 12:13:50 +0300

    Refactore code to remove await in loop

--- core/mosaic-dev-utils/get-folder-size.js
+++ core/mosaic-dev-utils/get-folder-size.js
@@ -1,37 +1,29 @@
-/* eslint-disable no-await-in-loop */
-/* eslint-disable no-restricted-syntax */
 const fs = require('fs');
 const path = require('path');
 
-const getFolderSizeFn = async (dirPath, size = 0) => {
-    let result = 0;
-    const fFiles = [];
+const getFolderSizeFn = async (dirPath) => {
+    const fileSizes = await fs.promises.readdir(dirPath, { withFileTypes: true, encoding: 'utf-8' })
+        .then((fFiles) => Promise.all(fFiles.map((file) => {
+            const filePath = path.join(dirPath, file.name);
 
-    for (const file of await fs.promises.readdir(dirPath, { withFileTypes: true, encoding: 'utf-8' })) {
-        const filePath = path.join(dirPath, file.name);
-
-        if (file.isFile()) {
-            fFiles.push(fs.promises.stat(filePath));
-        } else if (file.isDirectory()) {
-            fFiles.push(await getFolderSizeFn(filePath));
-        }
-    }
-
-    // Execute all promises and sum size depending on if file
-    // or result from previous calculation
-    await Promise.all(fFiles).then((values) => {
-        result = size + values.reduce((acc, file) => {
-            if (Number.isInteger(file)) {
-                return acc + file;
+            if (file.isFile()) {
+                return fs.promises.stat(filePath).then((file) => file.size);
             }
 
-            const { size } = file;
+            if (file.isDirectory()) {
+                return getFolderSizeFn(filePath);
+            }
 
-            return acc + size;
-        }, 0);
-    });
+            return 0;
+        })));
 
-    return result;
+    return fileSizes.reduce((acc, size) => {
+        if (size.length) {
+            return acc + size.reduce((a, b) => a + b);
+        }
+
+        return acc + size;
+    }, 0);
 };
 
 const getFolderSize = async (dirPath) => Math.round(await getFolderSizeFn(dirPath) / 1024 / 1024);
