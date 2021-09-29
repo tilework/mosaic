41a9dce4d150e3b7c42d027a37eddd429187347d
Okabe Rintaro <ejnshtein@gmail.com>
2021-09-29 13:50:52 +0300

    fix babel inject of aliases

--- integration/mosaic-config-injectors/lib/babel/add-aliases.js
+++ integration/mosaic-config-injectors/lib/babel/add-aliases.js
@@ -1,18 +1,49 @@
 const { alias } = require('./util/alias');
 
+const hasPlugin = (pluginName) => (plugin) => {
+    if (Array.isArray(plugin)) {
+        return plugin[0] === pluginName;
+    }
+
+    if (typeof plugin === 'string') {
+        return plugin === pluginName;
+    }
+};
+
+const babelPluginModuleResolverResolvedPath = require.resolve('babel-plugin-module-resolver');
+const babelPluginModuleResolverWithConfig = [
+    babelPluginModuleResolverResolvedPath,
+    {
+        root: 'src',
+        loglevel: 'silent',
+        alias
+    }
+];
+
 const addAliases = (babelConfig) => {
     if (!babelConfig.plugins) {
         babelConfig.plugins = [];
     }
 
-    babelConfig.plugins.push([
-        require.resolve('babel-plugin-module-resolver'), 
-        {
-            root: 'src',
-            loglevel: 'silent',
-            alias
+    if (babelConfig.plugins.some(hasPlugin(babelPluginModuleResolverResolvedPath))) {
+        const babelPluginModuleResolver = babelConfig.plugins.find(hasPlugin(require.resolve));
+
+        if (Array.isArray(babelPluginModuleResolver)) {
+            const options = babelPluginModuleResolver[1];
+
+            options.alias = {
+                ...options.alias,
+                ...alias
+            };
+        } else if (typeof babelPluginModuleResolver === 'string') {
+            babelConfig.plugins = babelConfig.plugins.filter((plugin => !hasPlugin(babelPluginModuleResolverResolvedPath)(plugin)));
+
+            babelConfig.plugins.push(babelPluginModuleResolverWithConfig);
         }
-    ]);
+    } else {
+        babelConfig.plugins.push(babelPluginModuleResolverWithConfig);
+    }
+
 
     return babelConfig;
 };
