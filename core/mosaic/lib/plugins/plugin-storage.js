8dd30d86d9e0b9c9712ecd23fc112647437bdcf1
alfredsgenkins <alfreds@scandipwa.com>
2022-12-29 15:00:43 +0200

    Fixed dynamic plugins merge problem

--- core/mosaic/lib/plugins/plugin-storage.js
+++ core/mosaic/lib/plugins/plugin-storage.js
@@ -15,7 +15,40 @@ class PluginStorage {
 
     addPlugins(importArray) {
         const newPlugins = generateConfig(importArray);
-        this.plugins = { ...this.plugins, ...newPlugins };
+
+        if (!this.plugins) {
+            this.plugins = {};
+        }
+
+        // vvv deep merge plugins
+        Object.entries(newPlugins).forEach(([namespace, pConf]) => {
+            if (!this.plugins[namespace]) {
+                this.plugins[namespace] = {};
+            }
+
+            Object.entries(pConf).forEach(([handlerType, cpConf]) => {
+                if (!this.plugins[namespace][handlerType]) {
+                    this.plugins[namespace][handlerType] = cpConf;
+                    return;
+                }
+
+                if (Array.isArray(cpConf)) {
+                    // ^^^ handles reduced plugins case
+                    this.plugins[namespace][handlerType].push(...cpConf);
+                }
+
+                // vvv handles regular plugin case
+                Object.entries(cpConf).forEach(([memberName, ccpConf]) => {
+                    if (!this.plugins[namespace][handlerType][memberName]) {
+                        this.plugins[namespace][handlerType][memberName] = ccpConf;
+                        return;
+                    }
+
+                    this.plugins[namespace][handlerType][memberName].push(...ccpConf);
+                });
+            });
+        });
+
         this.exposePlugins();
     }
 
