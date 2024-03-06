f53de818e463910ea83d421f64db775f50684d79
Aleksandrs Kondratjevs <68007919+AleksandrsKondratjevs@users.noreply.github.com>
2024-03-06 17:31:30 +0200

    Update env.js

--- integration/mosaic-cra-scripts/lib/env.js
+++ integration/mosaic-cra-scripts/lib/env.js
@@ -1,29 +1,9 @@
 const BASE_ENVIRONMENTS = [
-    'HOME',
-    'EXTENSION_PATH',
-    'BUILD_MODE',
-    'PATH',
-    'NODE',
-    'BASH_ENV',
-    'PWD',
-    'INIT_CWD',
-    'NEXTJS_PAGES',
-    'WDS_SOCKET_PORT',
-    'WDS_SOCKET_HOST',
-    'BROWSER',
-    'PUBLIC_URL',
-    'SKIP_PREFLIGHT_CHECK',
-    'FAST_REFRESH',
-    'FORCE_COLOR',
-    'NODE_ENV'
-];
-
-const MISC_ENVIRONMENTS = [
-    'DEBUG',
-    'DEBUG_FD',
     'GRAPHQL_ENDPOINT',
-    'REBEM_MOD_DELIM',
-    'REBEM_ELEM_DELIM'
+    'PUBLIC_URL',
+    'WDS_SOCKET_HOST',
+    'WDS_SOCKET_PORT',
+    'NODE_ENV'
 ];
 
 /**
@@ -52,7 +32,6 @@ const getEnvironments = (envs) => {
  */
 const getProcessEnv = () => [
     BASE_ENVIRONMENTS,
-    MISC_ENVIRONMENTS,
 ].map(getEnvironments).reduce((acc, val) => ({
     ...acc,
     ...val
